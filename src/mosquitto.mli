3d79bb8bbec090c3b803eedc165045d4ad3ee882
Gerd Stolpmann <gerd@figlyinc.com>
2021-07-08 17:20:42 +0200

    also support basic authentication

--- src/mosquitto.mli
+++ src/mosquitto.mli
@@ -21,6 +21,8 @@ val create : string -> bool -> (t, [>`EUnix of Unix.error]) Result.result
 
 val destroy : t -> unit
 
+val set_basic_auth : t -> string -> string -> unit, [>`EUnix of Unix.error]) Result.result
+
 val connect : t -> string -> int -> int -> (unit, [>`EUnix of Unix.error]) Result.result
 
 val reconnect : t -> (unit, [>`EUnix of Unix.error]) Result.result
